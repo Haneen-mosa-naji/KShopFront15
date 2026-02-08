5c27104d83c6bb3476d628c5e355f3359925375e
Haneen-mosa-naji <haneenmosa427@gmail.com>
2026-02-08 23:34:21 +0300

    Part1:Navbar2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { RouterProvider } from 'react-router-dom'
-import router from './Router'
+import router from './router.jsx'
 
 export default class App extends Component {
   render() {
